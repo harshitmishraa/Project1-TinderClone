b577243819d90d128d40c583d9fc112444ea648f
HARSHIT MISHRA <106691529+harshitmishraa@users.noreply.github.com>
2023-08-05 13:47:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import Home from './pages/Home'
+import Onboarding from './pages/Onboarding'
+import Dashboard from './pages/Dashboard'
+import { useCookies } from 'react-cookie'
+
+import { BrowserRouter , Routes , Route} from 'react-router-dom'
+const App =() => {
+  const [cookies , setCookie, removeCookie] = useCookies(['user'])
+
+  const authToken = cookies.AuthToken
+
+  return (
+    <BrowserRouter>
+    <Routes>
+
+    <Route path="/" element={<Home/>} />
+    {authToken && <Route path="/Dashboard" element={<Dashboard/>}/>}
+    {authToken && <Route path="/Onboarding" element={<Onboarding/>}/>}
+      
+    </Routes>
+     </BrowserRouter>
+    
+  );
+}
+
+export default App;
